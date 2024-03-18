260197489ec8d6523414c502890178282e87e081
Zelara <119063805+Zelara@users.noreply.github.com>
2024-03-18 13:00:50 -0400

    Enlever les carrées dans le menu en haut pour prendre moin d'espace

--- functions.php
+++ functions.php
@@ -1,3 +1,20 @@
 <?php
 add_theme_support('menus');
 add_theme_support('custom-logo');
+
+/**
+ * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
+ * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
+ * Dépendant de la condition initiale on peut filtrer un type particulier de requête
+ * Dans ce cas çi nous filtrons la requête de la page d'accueil
+ * @param WP_query  $query la requête principal de WP
+ */
+function _4w4_modifie_requete_principal($query)
+{
+    if ($query->is_home() && $query->is_main_query() && !is_admin()) {
+        $query->set('category_name', 'populaire');
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+    }
+}
+add_action('pre_get_posts', '_4w4_modifie_requete_principal');
