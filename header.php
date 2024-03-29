9070576e6975462e11675a96678cfa06b745eb75
Zelara <119063805+Zelara@users.noreply.github.com>
2024-03-29 18:35:55 -0400

    Re ajouter le footer. Il marche bien maintenant avec le header

--- header.php
+++ header.php
@@ -29,6 +29,6 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
                 </svg></label>
             <input id="chk_burger" type="checkbox">
-            <?php wp_nav_menu(array("container" => "nav")) ?>
+            <?php wp_nav_menu(array("container" => "nav", "menu" => "principal")) ?>
         </header>
     </div>
