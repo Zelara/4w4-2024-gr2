480aa5660db8ce96db25d2ce53a778f500e7bd64
Zelara <119063805+Zelara@users.noreply.github.com>
2024-04-02 20:29:40 -0400

    Changement de couleurs du contenu et changement de taille des caractères

--- front-page.php
+++ front-page.php
@@ -46,7 +46,7 @@
       <div class="contenu clr-agencement-rouge2">
         <h4 class="contenu-titre"><?php echo $category->name; ?></h4>
         <p><?php echo wp_trim_words($category->description, 10); ?></p>
-        <p class="contenu-article"><?php echo sprintf(_n('<span class="article-nombre">%d</span> Article', '<span class="article-nombre">%d</span> Articles', $category->count), $category->count); ?></p>
+        <p><?php echo sprintf(_n('<span class="article-nombre">%d</span> Article', '<span class="article-nombre">%d</span> Articles', $category->count), $category->count); ?></p>
         <p class="contenu-lien-article"><a href="<?php echo esc_url(add_query_arg('cat', $category->term_id, get_category_link($category->term_id))); ?>">Voir tous les articles</a></p>
       </div>
     <?php endforeach; ?>
