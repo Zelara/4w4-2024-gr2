ebd42f260b4a46eb7244b4460b7701cba23c37fe
Zelara <119063805+Zelara@users.noreply.github.com>
2024-04-11 16:38:07 -0400

    Petit changement au nom du lien au millieu

--- 404.php
+++ 404.php
@@ -9,10 +9,10 @@
     <div class="contenu-erreur">
       <div class="haut-erreur">
         <h1 class="titre-erreur">Erreur 404</h1>
-        <h2 class="texte-erreur">L'adresse que vous avez entrez n'existe pas. Vous pouvez revenir à la page d'accueil en cliquant sur ce lien</h2>
+        <h2 class="texte-erreur">Vous essayez d'accéder à une page qui n'existe pas</h2>
         <p class="texte-erreur">Pour revenir à la page d'accueil cliquer sur le lien suivant</p>
         <div class="boite-erreur">
-          <a href="<?php echo get_bloginfo('url'); ?>"> <?php echo get_bloginfo('name'); ?></a>
+          <a href="<?php echo get_bloginfo('url'); ?>"> Explorez le monde</a>
         </div>
         <?php get_search_form() ?>
         <?php wp_nav_menu(array("menu" => "categorie-liens")); ?>
