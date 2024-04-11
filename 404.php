48b127e3e4dad13731f76bae8c30fef63801f589
Zelara <119063805+Zelara@users.noreply.github.com>
2024-04-11 15:27:44 -0400

    Maquette général pour mobile fait

--- 404.php
+++ 404.php
@@ -6,10 +6,16 @@
 <!-- <h2>404.php</h2> -->
 <div id="erreur" class="global">
   <section>
-    <h2>Accueil</h2>
-    <div class="flexbox">
-      <p>L'adresse que vous avez entrez n'existe pas. Vous pouvez revenir à la page d'accueil en cliquant sur ce lien</p>
-      <a href="<?php echo get_bloginfo('url'); ?>"> <?php echo get_bloginfo('name'); ?></a>
+    <div class="contenu-erreur">
+      <?php the_post_thumbnail("thumbnail"); ?>
+      <h2 class="titre-erreur">Erreur 404</h2>
+      <p class="texte-erreur">L'adresse que vous avez entrez n'existe pas. Vous pouvez revenir à la page d'accueil en cliquant sur ce lien</p>
+      <p class="texte-erreur">Pour revenir à la page d'accueil cliquer sur le lien suivant</p>
+      <div class="boite-erreur">
+        <a href="<?php echo get_bloginfo('url'); ?>"> <?php echo get_bloginfo('name'); ?></a>
+      </div>
+      <?php get_search_form() ?>
+      <?php wp_nav_menu(array("menu" => "footer")); ?>
     </div>
   </section>
 </div>
