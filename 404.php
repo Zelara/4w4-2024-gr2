567e5f92f850dfab908dab30ea87da87a0d96c69
Zelara <119063805+Zelara@users.noreply.github.com>
2024-04-11 15:54:36 -0400

    Ajout d'une img qui apparait seulement en grand écran

--- 404.php
+++ 404.php
@@ -7,15 +7,24 @@
 <div id="erreur" class="global">
   <section>
     <div class="contenu-erreur">
-      <?php the_post_thumbnail("thumbnail"); ?>
-      <h1 class="titre-erreur">Erreur 404</h1>
-      <h2 class="texte-erreur">L'adresse que vous avez entrez n'existe pas. Vous pouvez revenir à la page d'accueil en cliquant sur ce lien</h2>
-      <p class="texte-erreur">Pour revenir à la page d'accueil cliquer sur le lien suivant</p>
-      <div class="boite-erreur">
-        <a href="<?php echo get_bloginfo('url'); ?>"> <?php echo get_bloginfo('name'); ?></a>
+      <div class="haut-erreur">
+        <h1 class="titre-erreur">Erreur 404</h1>
+        <h2 class="texte-erreur">L'adresse que vous avez entrez n'existe pas. Vous pouvez revenir à la page d'accueil en cliquant sur ce lien</h2>
+        <p class="texte-erreur">Pour revenir à la page d'accueil cliquer sur le lien suivant</p>
+        <div class="boite-erreur">
+          <a href="<?php echo get_bloginfo('url'); ?>"> <?php echo get_bloginfo('name'); ?></a>
+        </div>
+        <?php get_search_form() ?>
+        <?php wp_nav_menu(array("menu" => "footer")); ?>
       </div>
-      <?php get_search_form() ?>
-      <?php wp_nav_menu(array("menu" => "footer")); ?>
+      <div class="bas-erreur">
+        <!-- Ajout de l'img -->
+        <?php
+        $image_id = 112;
+        $image = wp_get_attachment_image($image_id, 'full', false, array('class' => 'img-erreur'));
+        echo $image;
+        ?>
+      </div>
     </div>
   </section>
 </div>
