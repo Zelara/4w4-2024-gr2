d8aa241963549d51e2b9bdd96aba5dbc34de1640
Zelara <119063805+Zelara@users.noreply.github.com>
2024-05-09 15:05:40 -0400

    Changer le titre pour le shortcode

--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php
 // Modèle de base index.php
+
 ?>
 
 <?php get_header(); ?>
@@ -58,11 +59,11 @@
 <!-- Section pour afficher un événement -->
 <div id="evenement" class="global">
   <section class="evenement__section">
-    <h2>Conseils</h2>
-	  <?php
-		// Appel du shortcode directement dans le fichier front-page.php
-		echo do_shortcode('[em_destination]');
-	  ?>
+    <h2>Traversées Culturelles</h2>
+    <?php
+    // Appel du shortcode directement dans le fichier front-page.php
+    echo do_shortcode('[em_destination]');
+    ?>
     <p>
       Lors de vos voyages, gardez toujours une copie numérique et papier de vos documents
       importants tels que votre passeport, votre visa, vos billets d'avion et vos
