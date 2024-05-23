9776619b5f253bf9e9a6508f59caab94cacfe836
Zelara <119063805+Zelara@users.noreply.github.com>
2024-05-23 16:31:38 -0400

    La page conférence est beaucoup mieux stylisé maintenant

--- template-evenement.php
+++ template-evenement.php
@@ -10,10 +10,18 @@
 <main class="site__main">
     <?php
     if (have_posts()) : the_post(); ?>
-        <h1><?= get_the_title(); ?></h1>
-        <?php the_content(); ?>
-        <p>L'adresse de l'evénement<?php the_field('adresse'); ?></p>
-        <p>La date et l'heure de l'événement<?php the_field('date_et_heure'); ?></p>
+        <div class="conference__texte">
+            <h1><?= get_the_title(); ?></h1>
+            <?php the_content(); ?>
+        </div>
+
+        <!-- dans la page tu peux aussi faire: La conférence aura lieu à [acf field = "adresse"] à [acf field="date_et_heure"] -->
+
+        <div class="conference__champs">
+            <p>L'adresse de l'evénement : <?php the_field('adresse'); ?> </p>
+            <p>La date et l'heure de l'événement : <?php the_field('date_et_heure'); ?> </p>
+            <!-- <p>L'heure de l'événement : <?php the_field('heure'); ?> </p> -->
+        </div>
     <?php endif; ?>
 </main><!-- #main -->
 <?php
