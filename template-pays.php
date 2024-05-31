a8bd4e948f27a3b4620bc54355b6133b898b1837
Zelara <119063805+Zelara@users.noreply.github.com>
2024-05-31 14:40:42 -0400

    ef-JL Essaie d'ajout les vagues et la carrousel, mais ne marche pas

--- template-pays.php
+++ template-pays.php
@@ -7,30 +7,33 @@
 
 <?php get_header(); ?>
 <main class="site__main">
-    <?php
-    if (have_posts()) : the_post(); ?>
-        <div class="pays__texte">
-            <h1>Les plus beaux pays</h1>
-            <p>Découvrez les plus belles destinations de voyage autour du monde.</p>
-        </div>
+    <div class="section-a">
+        <h1>Les plus magnifiques pays</h1>
+        <p>Laissez-vous emporter par la découverte des destinations de voyage les plus sublimes que notre monde a à offrir,
+            des endroits enchanteurs où la beauté naturelle se mêle harmonieusement à la richesse culturelle,
+            vous invitant ainsi à explorer, à apprendre et à vous émerveiller à chaque étape de votre périple.</p>
+    </div>
 
-        <?php get_template_part("gabarit/vague"); ?>
-
+    <!-- <div class="section-b">
         <div class="gallery">
             <img src="https://via.placeholder.com/150" alt="Placeholder Image 1">
             <img src="https://via.placeholder.com/150" alt="Placeholder Image 2">
             <img src="https://via.placeholder.com/150" alt="Placeholder Image 3">
             <img src="https://via.placeholder.com/150" alt="Placeholder Image 4">
         </div>
+    </div> -->
 
-        <?php get_template_part("gabarit/vague"); ?>
+    <?php get_template_part("gabarit/vague"); ?>
 
+    <div class="section-d">
         <div id="rest-api-content">
             <!-- Contenu généré par l'API REST sera inséré ici -->
-            <?php echo do_shortcode('[pays_menu]'); ?>
-        </div>
+            <?php echo do_shortcode('[carrousel]'); ?>
+    </div>
 
+    <div class="section-e">
         <?php get_template_part("gabarit/vague"); ?>
-    <?php endif; ?>
+    </div>
+
 </main><!-- #main -->
 <?php get_footer(); ?>
